e64257b726608587901fcc891ad7dcef111568c3
neerajkumar101 <neeraj.k@oodlestechnologies.com>
2017-08-24 12:38:33 +0530

    @ 12:38 pm 24th August 2017

--- test/sample.js
+++ test/sample.js
@@ -156,3 +156,128 @@ describe('Commodity Trading', () => {
         });
     });
 });
+
+describe('MyAsset Trading', () => {
+    // let adminConnection;
+    let businessNetworkConnection;
+
+    before(() => {
+        BrowserFS.initialize(new BrowserFS.FileSystem.InMemory());
+        const adminConnection = new AdminConnection({ fs: bfs_fs });
+        return adminConnection.createProfile('defaultProfile', {
+            type: 'embedded'
+        })
+            .then(() => {
+                return adminConnection.connect('defaultProfile', 'admin', 'adminpw');
+            })
+            .then(() => {
+                return BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'));
+            })
+            .then((businessNetworkDefinition) => {
+                return adminConnection.deploy(businessNetworkDefinition);
+            })
+            .then(() => {
+                businessNetworkConnection = new BusinessNetworkConnection({ fs: bfs_fs });
+                return businessNetworkConnection.connect('defaultProfile', 'my-network', 'admin', 'adminpw');
+            });
+    });
+
+    describe('#tradeMyAsset', () => {
+        it('should be able to trade a myAsset', () => {
+            const factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+            // create the traders
+            const dan = factory.newResource(NS, 'Trader', 'dan@email.com');
+            dan.firstName = 'Dan';
+            dan.lastName = 'Selman';
+
+            const simon = factory.newResource(NS, 'Trader', 'simon@email.com');
+            simon.firstName = 'Simon';
+            simon.lastName = 'Stone';
+
+            // create the myAsset
+            const myAsset = factory.newResource(NS, 'MyAsset', 'EMA');
+            myAsset.description = 'Corn';
+            myAsset.motoOfExchange = 'Euronext';
+            myAsset.quantity = 100;
+            myAsset.owner = factory.newRelationship(NS, 'Trader', dan.$identifier);
+
+            // create the trade transaction
+            const trade = factory.newTransaction(NS, 'TradeMyAsset');
+            trade.newOwner = factory.newRelationship(NS, 'Trader', simon.$identifier);
+            trade.myAsset = factory.newRelationship(NS, 'MyAsset', myAsset.$identifier);
+
+            // the owner should of the myAsset should be dan
+            myAsset.owner.$identifier.should.equal(dan.$identifier);
+
+            // create the second myAsset
+            const myAsset2 = factory.newResource(NS, 'MyAsset', 'XYZ');
+            myAsset2.description = 'Soya';
+            myAsset2.motoOfExchange = 'Chicago';
+            myAsset2.quantity = 50;
+            myAsset2.owner = factory.newRelationship(NS, 'Trader', dan.$identifier);
+
+            // register for events from the business network
+            businessNetworkConnection.on('event', (event) => {
+                console.log( 'Received event: ' + event.getFullyQualifiedIdentifier() + ' for myAsset ' + event.myAsset.getIdentifier() );
+            });
+
+            // Get the asset registry.
+            return businessNetworkConnection.getAssetRegistry(NS + '.MyAsset')
+                .then((assetRegistry) => {
+
+                    // add the myAssets to the asset registry.
+                    return assetRegistry.addAll([myAsset,myAsset2])
+                        .then(() => {
+                            return businessNetworkConnection.getParticipantRegistry(NS + '.Trader');
+                        })
+                        .then((participantRegistry) => {
+                            // add the traders
+                            return participantRegistry.addAll([dan, simon]);
+                        })
+                        .then(() => {
+                            // submit the transaction
+                            return businessNetworkConnection.submitTransaction(trade);
+                        })
+                        .then(() => {
+                            return businessNetworkConnection.getAssetRegistry(NS + '.MyAsset');
+                        })
+                        .then((assetRegistry) => {
+                            // re-get the myAsset
+                            return assetRegistry.get(myAsset.$identifier);
+                        })
+                        .then((newMyAsset) => {
+                            // the owner of the myAsset should now be simon
+                            newMyAsset.owner.$identifier.should.equal(simon.$identifier);
+                        })
+                        .then(() => {
+                            // use a query
+                            return businessNetworkConnection.query('selectMyAssetsByMotoOfExchange', {exchange : 'Euronext'});
+                        })
+                        .then((results) => {
+                            // check results
+                            results.length.should.equal(1);
+                            results[0].getIdentifier().should.equal('EMA');
+                        })
+                        .then(() => {
+                            // use another query
+                            return businessNetworkConnection.query('selectMyAssetsByOwner', {owner : 'resource:' + simon.getFullyQualifiedIdentifier()});
+                        })
+                        .then((results) => {
+                            //  check results
+                            results.length.should.equal(1);
+                            results[0].getIdentifier().should.equal('EMA');
+                        })
+                        .then(() => {
+                            // use a query
+                            return businessNetworkConnection.query('selectMyAsset');
+                        })
+                        .then((results) => {
+                            // check results, should only have 1 myAsset left
+                            results.length.should.equal(1);
+                            results[0].getIdentifier().should.equal('XYZ');
+                        });
+                });
+        });
+    });
+});
